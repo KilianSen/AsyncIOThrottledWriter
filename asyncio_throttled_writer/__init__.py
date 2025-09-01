9c9f0c916d8ca11d8a0e7096c45221abe2fdd1e5
KilianSen <kilian@senger.group>
2025-09-01 19:44:13 +0200

    bump version to 0.1.1 and update Python requirement to >=3.10

--- asyncio_throttled_writer/__init__.py
+++ asyncio_throttled_writer/__init__.py
@@ -6,5 +6,5 @@ A Python library providing a throttled stream writer for asyncio applications.
 
 from .throttled_writer import ThrottledStreamWriter
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["ThrottledStreamWriter"]
